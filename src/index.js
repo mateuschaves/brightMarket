3f6f870b69f187a87aed7bc63878daafa4fad830
Mateus Henrique <mateushenriquechaves1@gmail.com>
2019-11-06 23:57:40 -0300

    Push notifications

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import '~/config/ReactotronConfig';
 
@@ -11,6 +11,8 @@ import Colors from './constants/Colors';
 // import BottomNavigatiom from '~/navigation/BottomNavigatiom';
 import AuthNavigation from '~/navigation/AuthNavigation';
 
+import OneSignal from 'react-native-onesignal';
+
 const theme = {
   ...DefaultTheme,
   roundness: 2,
@@ -21,12 +23,44 @@ const theme = {
   },
 };
 
-const App = () => (
-  <PaperProvider theme={theme}>
-    <Provider store={store}>
-      <AuthNavigation />
-    </Provider>
-  </PaperProvider>
-);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
 
-export default App;
+    OneSignal.init('28d2e156-178e-4e22-868f-ad9403359ab8');
+  }
+
+  componentDidMount() {
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillMount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived = data => {
+    console.log(data);
+  };
+
+  onOpened = notification => {
+    console.log(notification);
+  };
+
+  onIds = id => {
+    console.log(id);
+  };
+
+  render() {
+    return (
+      <PaperProvider theme={theme}>
+        <Provider store={store}>
+          <AuthNavigation />
+        </Provider>
+      </PaperProvider>
+    );
+  }
+}
